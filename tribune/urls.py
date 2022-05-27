a7310a283172af1f7be4723a1c46a738cc958b34
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-27 15:23:49 +0300

    deployment

---
+++ tribune/urls.py
@@ -0,0 +1,34 @@
+from django.conf.urls import url,include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('news.urls'))
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+# from django.conf.urls import url,include
+# from django.contrib import admin
+
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+#     url(r'^news/',include('news.urls'))
+# ]
