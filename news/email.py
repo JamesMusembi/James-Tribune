39591b6cff9fdd78d64c101b423f374416b55dd4
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-01 23:08:27 +0300

    sending email

--- news/email.py
+++ news/email.py
@@ -4,4 +4,4 @@ from django.template.loader import render_to_string
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
     subject = 'Welcome to the MoringaTribune NewsLetter'
-    sender = 'james@moringaschool.com'
+    sender = 'james.musembi@student.moringaschool.com'
