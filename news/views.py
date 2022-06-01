b52e4803b9fc3e9d4d7afb227f78ae0cf84e2415
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-01 23:49:43 +0300

    sending email

--- news/views.py
+++ news/views.py
@@ -1,30 +1,44 @@
 from django.shortcuts import render,redirect
-import datetime as dt
-from .models import Article, NewsLetterRecipients
-from .forms import NewsLetterForm
 from django.http import HttpResponse, Http404,HttpResponseRedirect
+import datetime as dt
+from .models import Article,NewsLetterRecipients
+from .forms import NewsLetterForm,NewArticleForm
 from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
+from .forms import NewArticleForm, NewsLetterForm
+from django.http import JsonResponse
+
+
 
 # Create your views here.
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    
-    if request.method == 'POST':
-        form = NewsLetterForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['your_name']
-            email = form.cleaned_data['email']
-
-            recipient = NewsLetterRecipients(name = name,email =email)
-            recipient.save()
-            send_welcome_email(name,email)
-
-            HttpResponseRedirect('news_today')
-            #.................
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
+    # if request.method == 'POST':
+    #     form = NewsLetterForm(request.POST)
+    #     if form.is_valid():
+    #         # print('valid')
+    #         name = form.cleaned_data['your_name']
+    #         email = form.cleaned_data['email']
+    #         recipient = NewsLetterRecipients(name = name,email =email)
+    #         recipient.save()
+    #         send_welcome_email(name,email)
 
 
+    #         HttpResponseRedirect('news_today')
+    # else:
+    form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html',{'date': date,"news":news,"letterForm":form})
+
+def newsletter(request):
+    name = request.POST.get('your_name')
+    email = request.POST.get('email')
+
+    recipient = NewsLetterRecipients(name=name, email=email)
+    recipient.save()
+    send_welcome_email(name, email)
+    data = {'success': 'You have been successfully added to mailing list'}
+    return JsonResponse(data)
 
 def past_days_news(request,past_date):
     
@@ -43,7 +57,7 @@ def past_days_news(request,past_date):
     return render(request, 'all-news/past-news.html', {"date":date,"news":news})
 
 def search_results(request):
-
+    
     if 'article' in request.GET and request.GET["article"]:
         search_term = request.GET.get("article")
         searched_articles = Article.search_by_title(search_term)
@@ -54,7 +68,8 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html',{"message":message})
-    
+
+@login_required(login_url='/accounts/login/')    
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
@@ -62,3 +77,16 @@ def article(request,article_id):
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
 
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('newsToday')
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
