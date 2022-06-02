bbff6cb5b258638783714c8b21259ec2fe8ce2a2
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-03 00:07:27 +0300

    creating editor

--- news/admin.py
+++ news/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from.models import Editor,Article,tags
 
+from django.contrib import admin
+from .models import Editor,Article,tags
+
+# Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
     filter_horizontal =('tags',)
 
-# Register your models here.
 admin.site.register(Editor)
 admin.site.register(Article,ArticleAdmin)
-admin.site.register(tags) 
+admin.site.register(tags)
--- news/models.py
+++ news/models.py
@@ -3,7 +3,6 @@ import datetime as dt
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
-
 # Create your models here.
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
@@ -27,14 +26,13 @@ class tags(models.Model):
         return self.name
 
 class Article(models.Model):
-    title = models.CharField(max_length=60)
-    post = models.TextField()
+    title = models.CharField(max_length =60)
+    post = HTMLField()
     editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to='articles/', blank=True)
-    editor = models.ForeignKey(User,on_delete=models.CASCADE) 
-    
+    article_image = models.ImageField(upload_to = 'articles/')
+
     def __str__(self):
         return self.title
     @classmethod
@@ -52,6 +50,7 @@ class Article(models.Model):
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
         return news
+
 class NewsLetterRecipients(models.Model):
     name = models.CharField(max_length = 30)
     email = models.EmailField()
